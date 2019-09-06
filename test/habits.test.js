68f7b6edb5050827c4213f7121e016b9d0d87199
Jackmc <jackmc1122@gmail.com>
2019-09-06 10:18:48 -0700

    Add get and post route for habit

---
+++ test/habits.test.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Habit = require('../lib/models/Habit');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('creates a habit', () => {
+    return request(app)
+      .post('/api/v1/habits')
+      .send({ habit: 'somehabit', description: 'somedescription' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          user: expect.any(String),
+          habit: 'somehabit',
+          description: 'somedescription',
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  it('gets all habits', async() => {
+    const habit = await Habit.create({ 
+      habit: 'somehabit',
+      description: 'somedescription',
+      user: 'someuser'
+    });
+    return request(app)
+      .get('/api/v1/habits')
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+        expect(res.body[0]).toEqual({
+          _id: habit._id.toString(),
+          habit: 'somehabit',
+          description: 'somedescription',
+          user: 'someuser',
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+});
